0f78ddfcebec0349d9bd17a7a825653fb924d32f
Aayushi Arya <101689725+aryaa0502@users.noreply.github.com>
2025-01-09 12:15:37 +0530

    initial commit

---
+++ services/meeting.service.js
@@ -0,0 +1,112 @@
+const {meeting} = require("../models/meeting.model");
+const {meetingUser} = require("../models/meeting-user.model");
+
+async function getAllMeetingUsers (meetId, callback){
+    meetingUser.find({meetingId: meetId})
+    .then((response) => {
+        return callback(null, response);
+    })
+    .catch((error) => {
+        return callback(error);
+    });
+}
+
+async function startMeeting(params, callback){
+    const meetingSchema = new meeting(params);
+    meetingSchema
+    .save()
+    .then(async (response) => {
+        return callback(null, response);
+    })
+    .catch((error) => {
+        return callback(error);
+    });
+}
+
+async function joinMeeting(params, callback){
+    const meetingUserModel = new meetingUser(params);
+
+    meetingUserModel
+    .save()
+    .then(async (response) => {
+        await meeting.findOneAndUpdate(
+            {id: params.meetingId},
+            {$addToSet: {"meetingUsers": meetingUserModel}}
+        );
+        return callback(null, response);
+    })
+    .catch((error) => {
+        return callback(error);
+    });
+}
+
+async function isMeetingPresent(meetingId, callback){
+    meeting.findById(meetingId)
+    .populate("meetingUsers", "MeetingUser")
+    .then((response) => {
+        if(!response) callback("Invalid meeting ID");
+        else callback(null, true);
+    })
+    .catch((error) => {
+        return callback(error, false);
+    });
+}
+
+async function checkMeetingExists(meetingId, callback){
+    meeting.findById(meetingId)
+    .populate("meetingUsers", "MeetingUser")
+    .then((response) => {
+        if(!response) callback("Invalid meeting ID");
+        else callback(null, response);
+    })
+    .catch((error) => {
+        return callback(error, false);
+    });
+}
+
+async function getMeetingUser(params, callback){
+    const { meetingId, userId } = params;
+
+    meetingUser.find({meetingId, userId})
+    .then((response) => {
+        return callback(null, response[0]);
+    })
+    .catch((error) => {
+        return callback(error);
+    });
+}
+
+async function updateMeetingUser(params, callback){
+    meetingUser
+    .updateOne({ userId: params.userid}, {$set: params}, {new: true})
+    .then((response) => {
+        return callback(null, response);
+    })
+    .catch((error) => {
+        return callback(error);
+    });
+}
+
+async function getUserBySocketId(params, callback){
+    const { meetingId, socketId } = params;
+    meetingUser.find({meetingId, socketId})
+    .limit(1)
+    .then((response) => {
+        return callback(null, response);
+    })
+    .catch((error) => {
+        return callback(error);
+    });
+}
+
+module.exports = {
+    getAllMeetingUsers,
+    startMeeting,
+    joinMeeting,
+    isMeetingPresent,
+    checkMeetingExists,
+    getMeetingUser,
+    updateMeetingUser,
+    getUserBySocketId
+}
+
