002eb9fe3571ac89883ea1a2f5c4471dc832d47f
Aayushi Arya <101689725+aryaa0502@users.noreply.github.com>
2025-01-10 11:26:07 +0530

    Update app.config.js

--- config/app.config.js
+++ config/app.config.js
@@ -1,5 +1,6 @@
 const MONGO_DB_CONFIG = {
-    DB: "mongodb://34.93.141.164:27017/meeting-app"
+    DB: "mongodb://127.0.0.1:27017/meeting-app"
+    // DB: "mongodb://34.93.141.164:27017/meeting-app"
 };
 
 module.exports = {
